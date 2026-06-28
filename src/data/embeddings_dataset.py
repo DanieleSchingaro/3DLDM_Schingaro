1bbb50fff9c4736e3754def40d8219a22b601e09
DanieleSchingaro <d.schingaro04@gmail.com>
2026-06-28 15:33:33 +0000

    modifiche ai path di input/output dei file addattandoli a v3.

--- src/data/embeddings_dataset.py
+++ src/data/embeddings_dataset.py
@@ -11,8 +11,8 @@ import json
 from collections import Counter
 
 SPLITS_PATH="data/splits/dataset.json"
-EMBEDDINGS_DIR="data/processed/embeddings_v2"
-OUTPUT_PATH="data/splits/embeddings_dataset_v2.json"
+EMBEDDINGS_DIR="data/processed/embeddings_v3"
+OUTPUT_PATH="data/splits/embeddings_dataset_v3.json"
 
 #mapping per fonte dataset
 DATASET_SOURCES={
