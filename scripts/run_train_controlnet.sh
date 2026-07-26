07b8466901cc960bb7fbe4eba69eccc903bdaed8
DanieleSchingaro <d.schingaro04@gmail.com>
2026-07-26 17:55:06 +0000

    file .sh utile a lanciare il training della controlnet.

---
+++ scripts/run_train_controlnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /mnt/data/home-ubuntu/work/medical-3D-Rflow-Maisi-Schingaro/.venv/bin/activate
+cd /mnt/data/home-ubuntu/work/medical-3D-Rflow-Maisi-Schingaro
+mkdir -p logs
+
+# stampa info ambiente
+echo "Start: $(date)"
+echo "GPU disponibili: $(nvidia-smi --list-gpus | wc -l)"
+
+MASTER_PORT=$((29000 + RANDOM % 2000))
+torchrun --nproc_per_node=4 --master_port=$MASTER_PORT -m src.training.train_controlnet \
+    2>&1 | tee logs/train_controlnet_v6_$(date +%Y%m%d_%H%M).log
+
+echo "End: $(date)"
